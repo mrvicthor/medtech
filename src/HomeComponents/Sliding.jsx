9f451a25eb3ed6fa34bfde6bee84645660159113
Victor Eleanya <victoreleanya89@gmail.com>
2024-09-16 12:59:57 +0100

    feat(Sliding.jsx): change background image

--- src/HomeComponents/Sliding.jsx
+++ src/HomeComponents/Sliding.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Box } from '@chakra-ui/react';
-import Slider from 'react-slick';
-
+import React from "react";
+import { Box } from "@chakra-ui/react";
+import Slider from "react-slick";
+// import { fetchBinanceData } from "../utils/fetchBinanceData";
 
 // Settings for the slider
 const settings = {
@@ -17,49 +17,53 @@ const settings = {
 };
 
 export default function Sliding() {
-  
-  const [slider, setSlider] = React.useState (null);
+  const [slider, setSlider] = React.useState(null);
 
   //  images used in the slide
+  const prevDoctorImage =
+    "https://wallpapers.com/images/featured/doctor-kwucobzhm0etbcwy.jpg";
+
   const cards = [
-    'https://wallpapers.com/images/featured/doctor-kwucobzhm0etbcwy.jpg'
+    "https://wallpapers.com/images/featured/flying-doctor-strange-minimalist-zcm19c5hjix7kc1e.jpg",
   ];
 
   return (
-    <Box
-      position={'relative'}
-      height={'600px'}
-      width={'full'}
-      overflow={'hidden'}>
-      <link
-        rel="stylesheet"
-        type="text/css"
-        charSet="UTF-8"
-        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-      />
-      <link
-        rel="stylesheet"
-        type="text/css"
-        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-      />
+    <>
       <Box
-             position="absolute"
-           />
-      <Slider {...settings} ref={(slider) => setSlider(slider)}>
-        {cards.map((url, index) => (
-          <Box
-            key={index}
-            height={'6xl'}
-            position="relative"
-            backgroundPosition="center"
-            backgroundRepeat="no-repeat"
-            backgroundSize="cover"
-            backgroundImage={`url(${url})`}
-            
-          />
-
-        ))}
-      </Slider>
-    </Box>
+        position={"relative"}
+        height={"600px"}
+        width={"full"}
+        overflow={"hidden"}
+      >
+        <link
+          rel="stylesheet"
+          type="text/css"
+          charSet="UTF-8"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+        />
+        <Box position="absolute" />
+        <Slider {...settings} ref={(slider) => setSlider(slider)}>
+          {cards.map((url, index) => (
+            <>
+              <Box
+                key={index}
+                height={"6xl"}
+                position="relative"
+                backgroundPosition="center"
+                backgroundRepeat="no-repeat"
+                backgroundSize="cover"
+                backgroundImage={`url(${url})`}
+              />
+              <h1>vic</h1>
+            </>
+          ))}
+        </Slider>
+      </Box>
+    </>
   );
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,75 +1,120 @@
-import { Flex,Box, Stack, Text, Button, useBreakpointValue } from '@chakra-ui/react'
-import HomeTop from '../HomeComponents/HomeTop';
-import HomeServices from '../HomeComponents/HomeService';
-import HomeDoctor from '../HomeComponents/HomeDoctor';
-import Sliding from '../HomeComponents/Sliding';
-import { HomeFAQ } from '../HomeComponents/HomeFAQ';
+import { useState, useEffect } from "react";
+import {
+  Flex,
+  Box,
+  Stack,
+  Text,
+  Button,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+import HomeTop from "../HomeComponents/HomeTop";
+import HomeServices from "../HomeComponents/HomeService";
+import HomeDoctor from "../HomeComponents/HomeDoctor";
+import Sliding from "../HomeComponents/Sliding";
+import { HomeFAQ } from "../HomeComponents/HomeFAQ";
 
-function Home(){   
+function Home() {
+  const redirectTo = () => {
+    window.location.href = "/services";
+  };
 
-    const redirectTo = () => {
-        window.location.href = '/services'; 
-    }
+  const [price, setPrice] = useState(null);
 
-    return(  
-        <Box
-            maxWidth="100%" mx="auto"
-            w={"full"}
-            overflow="hidden"
-            position="relative"
-        >
-            <Box
-                position="absolute"
-                top={0}
-                left={0}
-                right={0}
-                w="50%"
-            />
+  useEffect(() => {
+    const fetchBinanceData = async () => {
+      const response = await fetch(
+        "https://api.binance.com/api/v3/ticker/price?symbol=BNBBTC"
+      );
+      try {
+        const data = await response.json();
+        console.log("price of data", data.price);
+        setPrice(data.price);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchBinanceData();
+  }, []);
+
+  return (
+    <>
+      <Text>BNB: {price}</Text>
+      <Box
+        maxWidth="100%"
+        mx="auto"
+        w={"full"}
+        overflow="hidden"
+        position="relative"
+      >
+        <Box position="absolute" top={0} left={0} right={0} w="50%" />
+        <Box>
+          <Box position="relative" height="100%">
             <Box>
-            <Box position="relative" height="100%">
-                <Box>
-                    <Flex>
-                        <Sliding />
-                        <Box position="absolute" zIndex={5}  left={0} right={0} p={4}>
-                            <Stack maxW={'2xl'} align={'flex-start'} spacing={6} alignSelf={'flex-start'} textAlign={"left"} mt={200}>
-                                <Text
-                                    color={'white'}       
-                                    fontWeight={"semibold"}
-                                    lineHeight={1.2}
-                                    fontSize={useBreakpointValue({ base: '1xl', md: '1xl' })}>
-                                    WE GIVE YOU THE BEST
-                                </Text>
-                                <Text color={'#13d6a8'}
-                                    fontWeight={700}
-                                    lineHeight={1.2}
-                                    fontSize={useBreakpointValue({ base: '3xl', md: '5xl' })}>
-                                    Care Better Together
-                                </Text>
-                                <Stack direction={{"base":"column","sm":"column","md":"row"}}>
-                                    <Button onClick={redirectTo} borderRadius={0} color={"white"}  bg="rgba(1, 213, 162, 1)" variant='solid' opacity={1} size={'lg'} fontSize={'xs'} >SEE OUR SERVICES</Button>
-                                </Stack>
-                            </Stack>
-                        </Box>
-                    </Flex>
+              <Flex>
+                <Sliding />
+                <Box position="absolute" zIndex={5} left={0} right={0} p={4}>
+                  <Stack
+                    maxW={"2xl"}
+                    align={"flex-start"}
+                    spacing={6}
+                    alignSelf={"flex-start"}
+                    textAlign={"left"}
+                    mt={200}
+                  >
+                    <Text
+                      color={"white"}
+                      fontWeight={"semibold"}
+                      lineHeight={1.2}
+                      fontSize={useBreakpointValue({ base: "1xl", md: "1xl" })}
+                    >
+                      WE GIVE YOU THE BEST
+                    </Text>
+                    <Text
+                      color={"#13d6a8"}
+                      fontWeight={700}
+                      lineHeight={1.2}
+                      fontSize={useBreakpointValue({ base: "3xl", md: "5xl" })}
+                    >
+                      Care Better Together
+                    </Text>
+                    <Stack
+                      direction={{ base: "column", sm: "column", md: "row" }}
+                    >
+                      <Button
+                        onClick={redirectTo}
+                        borderRadius={0}
+                        color={"white"}
+                        bg="rgba(1, 213, 162, 1)"
+                        variant="solid"
+                        opacity={1}
+                        size={"lg"}
+                        fontSize={"xs"}
+                      >
+                        SEE OUR SERVICES
+                      </Button>
+                    </Stack>
+                  </Stack>
                 </Box>
+              </Flex>
             </Box>
+          </Box>
         </Box>
 
         <Box>
-            <HomeTop />
+          <HomeTop />
         </Box>
         <Box>
-            <HomeServices />
+          <HomeServices />
         </Box>
         <Box padding={10}>
-            <HomeDoctor />
+          <HomeDoctor />
         </Box>
         <Box>
-            <HomeFAQ />
+          <HomeFAQ />
         </Box>
-    
-    </Box>
-    )
+      </Box>
+    </>
+  );
 }
 
 export default Home;
